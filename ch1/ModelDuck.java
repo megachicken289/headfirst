136caebe78d177f85f816fe27cf64d4b84db8f40
Frank_XPTHQ <megachicken289@me.com>
2019-01-10 19:25:27 -0500

    move designPattern stuff to designated dir take 2

--- ch1/ModelDuck.java
+++
@@ -1,9 +0,0 @@
-public class ModelDuck extends Duck {
-	public ModelDuck() {
-		quackBehavior = new Quack();
-		flyBehavior = new FlyNoWay();
-	}
-	public void display() {
-		System.out.println("I'm a model duck");
-	}
-}
